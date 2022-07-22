43dc6b046fef5946743a12f9a1ba9d93e5ad4f84
sanjay0001 <75019244+sanjay0001@users.noreply.github.com>
2022-07-23 00:17:10 +0530

    Update urls.py

--- AskIt/urls.py
+++ AskIt/urls.py
@@ -13,21 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
+
 from django.contrib import admin
-from django.urls import path,include,re_path
+from django.urls import path,include
 
 from django.conf import settings
-from django.conf.urls.static import static
-#import url
-from django.views.static import serve
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
     path('home/', include('login.urls')),
-    #  path('register/', include('login.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
